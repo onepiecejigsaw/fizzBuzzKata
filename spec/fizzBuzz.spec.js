39bed4f7a4ddc9536482b3bbb5b768514f58b651
andy <andy@onepiecejigsaw.com>
2020-02-21 17:13:26 +0000

    Completed a set of property-based tests

--- spec/fizzBuzz.spec.js
+++ spec/fizzBuzz.spec.js
@@ -142,10 +142,55 @@ describe("FizzBuzz", () => {
   describe("#converting a range of integers", () => {
     it("returns a string with Fizz in it if the input is a multiple of 3", () => {
       fc.assert(
-        fc.property(fc.integer(0, 10000000000), n => {
+        fc.property(fc.integer(0, 1000000000), n => {
           const result = FizzBuzz.convertSingleInteger(n);
           if (n % 3 === 0) expect(result.indexOf("Fizz")).toBeGreaterThan(-1);
-        }), {numRuns: 100, verbose: true}
+        }),
+        { numRuns: 100, verbose: true }
+      );
+    });
+
+    it("returns a string with Fizz in it if the input contains a 3", () => {
+      fc.assert(
+        fc.property(fc.integer(0, 1000000000), n => {
+          const result = FizzBuzz.convertSingleInteger(n);
+          if (n.toString().indexOf("3") > -1)
+            expect(result.indexOf("Fizz")).toBeGreaterThan(-1);
+        })
+      );
+    });
+
+    it("returns a string with Buzz in it if the input is a multiple of 5", () => {
+      fc.assert(
+        fc.property(fc.integer(0, 1000000000), n => {
+          const result = FizzBuzz.convertSingleInteger(n);
+          if (n % 5 === 0) expect(result.indexOf("Buzz")).toBeGreaterThan(-1);
+        })
+      );
+    });
+
+    it("returns a string with Buzz in it if the input contains a 5", () => {
+      fc.assert(
+        fc.property(fc.integer(0, 1000000000), n => {
+          const result = FizzBuzz.convertSingleInteger(n);
+          if (n.toString().indexOf("5") > -1)
+            expect(result.indexOf("Buzz")).toBeGreaterThan(-1);
+        })
+      );
+    });
+
+    it("returns the input if the input is not a multiple of 3 or 5, and does not contain a 3 or 5", () => {
+      fc.assert(
+        fc.property(fc.integer(0, 1000000000), n => {
+          const result = FizzBuzz.convertSingleInteger(n);
+          if (
+            n % 3 !== 0 &&
+            n % 5 !== 0 &&
+            n.toString().indexOf("3") === -1 &&
+            n.toString().indexOf("5") === -1
+          )
+            expect(result).toBe(n);
+        })
       );
     });
   });
