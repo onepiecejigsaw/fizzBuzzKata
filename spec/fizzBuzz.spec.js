e0928462ebcdefa38b256bd7c5eab9e0316151c5
andy <andy@onepiecejigsaw.com>
2020-02-27 20:26:16 +0000

    Added jest and refactored, removing unnecessary tests

--- spec/fizzBuzz.spec.js
+++ spec/fizzBuzz.spec.js
@@ -2,196 +2,62 @@ const FizzBuzz = require("../src/FizzBuzz");
 const fc = require("fast-check");
 
 describe("FizzBuzz", () => {
-  describe("#converting single integer", () => {
-    it("returns 1 when converting the integer 1", () => {
-      var integerToConvert = 1;
-      const expectedResult = 1;
 
-      expect(FizzBuzz.convertSingleInteger(integerToConvert)).toEqual(
-        expectedResult
-      );
-    });
-
-    it("returns 2 when converting the integer 2", () => {
-      var integerToConvert = 2;
-      const expectedResult = 2;
-
-      expect(FizzBuzz.convertSingleInteger(integerToConvert)).toEqual(
-        expectedResult
-      );
-    });
-
-    it("returns Fizz when converting the integer 3", () => {
-      var integerToConvert = 3;
-      const expectedResult = "Fizz";
-
-      expect(FizzBuzz.convertSingleInteger(integerToConvert)).toEqual(
-        expectedResult
-      );
-    });
-
-    it("returns 4 when converting the integer 4", () => {
-      var integerToConvert = 4;
-      const expectedResult = 4;
-
-      expect(FizzBuzz.convertSingleInteger(integerToConvert)).toEqual(
-        expectedResult
-      );
-    });
-
-    it("returns Buzz when converting the integer 5", () => {
-      var integerToConvert = 5;
-      const expectedResult = "Buzz";
-
-      expect(FizzBuzz.convertSingleInteger(integerToConvert)).toEqual(
-        expectedResult
-      );
-    });
-
-    it("returns Fizz when converting the integer 6", () => {
-      var integerToConvert = 6;
-      const expectedResult = "Fizz";
-
-      expect(FizzBuzz.convertSingleInteger(integerToConvert)).toEqual(
-        expectedResult
-      );
-    });
-
-    it("returns Buzz when converting the integer 10", () => {
-      var integerToConvert = 10;
-      const expectedResult = "Buzz";
-
-      expect(FizzBuzz.convertSingleInteger(integerToConvert)).toEqual(
-        expectedResult
-      );
-    });
-
-    it("returns Buzz when converting the integer 13", () => {
-      var integerToConvert = 13;
-      const expectedResult = "Fizz";
-
-      expect(FizzBuzz.convertSingleInteger(integerToConvert)).toEqual(
-        expectedResult
-      );
-    });
-
-    it("returns FizzBuzz when converting the integer 15", () => {
-      var integerToConvert = 15;
-      const expectedResult = "FizzBuzz";
-
-      expect(FizzBuzz.convertSingleInteger(integerToConvert)).toEqual(
-        expectedResult
-      );
-    });
-
-    it("returns Buzz when converting the integer 23", () => {
-      var integerToConvert = 23;
-      const expectedResult = "Fizz";
-
-      expect(FizzBuzz.convertSingleInteger(integerToConvert)).toEqual(
-        expectedResult
-      );
-    });
-
-    it("returns FizzBuzz when converting the integer 30", () => {
-      var integerToConvert = 30;
-      const expectedResult = "FizzBuzz";
-
-      expect(FizzBuzz.convertSingleInteger(integerToConvert)).toEqual(
-        expectedResult
-      );
-    });
-
-    it("returns FizzBuzz when converting the integer 31", () => {
-      var integerToConvert = 31;
-      const expectedResult = "Fizz";
-
-      expect(FizzBuzz.convertSingleInteger(integerToConvert)).toEqual(
-        expectedResult
-      );
-    });
-
-    it("returns FizzBuzz when converting the integer 35", () => {
-      var integerToConvert = 35;
-      const expectedResult = "FizzBuzz";
-
-      expect(FizzBuzz.convertSingleInteger(integerToConvert)).toEqual(
-        expectedResult
-      );
-    });
-
-    it("returns FizzBuzz when converting the integer 51", () => {
-      var integerToConvert = 51;
-      const expectedResult = "FizzBuzz";
-
-      expect(FizzBuzz.convertSingleInteger(integerToConvert)).toEqual(
-        expectedResult
-      );
-    });
-
-    it("returns Buzz when converting the integer 52", () => {
-      var integerToConvert = 52;
-      const expectedResult = "Buzz";
-
-      expect(FizzBuzz.convertSingleInteger(integerToConvert)).toEqual(
-        expectedResult
-      );
-    });
-  });
+  const runTenThousandTests = predicate => {
+    fc.assert(
+        fc.property(fc.integer(0, 1000000000), predicate)
+        , {numRuns: 10000, verbose: true}
+    )
+  };
 
   describe("#converting a range of integers", () => {
-    it("returns a string with Fizz in it if the input is a multiple of 3", () => {
-      fc.assert(
-        fc.property(fc.integer(0, 1000000000), n => {
-          const result = FizzBuzz.convertSingleInteger(n);
-          if (n % 3 === 0) expect(result.indexOf("Fizz")).toBeGreaterThan(-1);
-        }),
-        { numRuns: 100, verbose: true }
-      );
-    });
+        it("returns a string with Fizz in it if the input is a multiple of 3", () => {
+            runTenThousandTests(
+                n => {
+                    const result = FizzBuzz.convertSingleInteger(n);
+                    if (n % 3 === 0) expect(result.indexOf("Fizz")).toBeGreaterThan(-1);
+                }
+            );
+        });
 
-    it("returns a string with Fizz in it if the input contains a 3", () => {
-      fc.assert(
-        fc.property(fc.integer(0, 1000000000), n => {
-          const result = FizzBuzz.convertSingleInteger(n);
-          if (n.toString().indexOf("3") > -1)
-            expect(result.indexOf("Fizz")).toBeGreaterThan(-1);
-        })
-      );
-    });
+        it("returns a string with Fizz in it if the input contains a 3", () => {
+            runTenThousandTests(n => {
+                    const result = FizzBuzz.convertSingleInteger(n);
+                    if (n.toString().indexOf("3") > -1)
+                        expect(result.indexOf("Fizz")).toBeGreaterThan(-1);
+                }
+            );
+        });
 
-    it("returns a string with Buzz in it if the input is a multiple of 5", () => {
-      fc.assert(
-        fc.property(fc.integer(0, 1000000000), n => {
-          const result = FizzBuzz.convertSingleInteger(n);
-          if (n % 5 === 0) expect(result.indexOf("Buzz")).toBeGreaterThan(-1);
-        })
-      );
-    });
+        it("returns a string with Buzz in it if the input is a multiple of 5", () => {
+            runTenThousandTests(n => {
+                    const result = FizzBuzz.convertSingleInteger(n);
+                    if (n % 5 === 0) expect(result.indexOf("Buzz")).toBeGreaterThan(-1);
+                }
+            );
+        });
 
-    it("returns a string with Buzz in it if the input contains a 5", () => {
-      fc.assert(
-        fc.property(fc.integer(0, 1000000000), n => {
-          const result = FizzBuzz.convertSingleInteger(n);
-          if (n.toString().indexOf("5") > -1)
-            expect(result.indexOf("Buzz")).toBeGreaterThan(-1);
-        })
-      );
-    });
+        it("returns a string with Buzz in it if the input contains a 5", () => {
+            runTenThousandTests(n => {
+                    const result = FizzBuzz.convertSingleInteger(n);
+                    if (n.toString().indexOf("5") > -1)
+                        expect(result.indexOf("Buzz")).toBeGreaterThan(-1);
+                }
+            );
+        });
 
-    it("returns the input if the input is not a multiple of 3 or 5, and does not contain a 3 or 5", () => {
-      fc.assert(
-        fc.property(fc.integer(0, 1000000000), n => {
-          const result = FizzBuzz.convertSingleInteger(n);
-          if (
-            n % 3 !== 0 &&
-            n % 5 !== 0 &&
-            n.toString().indexOf("3") === -1 &&
-            n.toString().indexOf("5") === -1
-          )
-            expect(result).toBe(n);
-        })
-      );
+        it("returns the input if the input is not a multiple of 3 or 5, and does not contain a 3 or 5", () => {
+            runTenThousandTests(n => {
+                    const result = FizzBuzz.convertSingleInteger(n);
+                    if (
+                        n % 3 !== 0 &&
+                        n % 5 !== 0 &&
+                        n.toString().indexOf("3") === -1 &&
+                        n.toString().indexOf("5") === -1
+                    )
+                        expect(result).toBe(n);
+                }
+            );
+        });
     });
-  });
 });
--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,30 +1,27 @@
 var FizzBuzz = {
-    convertSingleInteger: function(integerToConvert) {
-        if(this.shouldBeConvertedToFizz(integerToConvert) && this.shouldBeConvertedToBuzz(integerToConvert))
+    convertSingleInteger: function (integerToConvert) {
+        if (this.shouldBeConvertedToFizz(integerToConvert) && this.shouldBeConvertedToBuzz(integerToConvert))
             return "FizzBuzz";
-        if(this.shouldBeConvertedToFizz(integerToConvert))
+        if (this.shouldBeConvertedToFizz(integerToConvert))
             return "Fizz";
-        if(this.shouldBeConvertedToBuzz(integerToConvert))
+        if (this.shouldBeConvertedToBuzz(integerToConvert))
             return "Buzz";
         return integerToConvert;
     },
 
-    shouldBeConvertedToFizz: function(integerToConvert) {
-        return (integerToConvert%3 === 0 || integerToConvert.toString().indexOf("3") > -1);
+    shouldBeConvertedToFizz: function (integerToConvert) {
+        return (integerToConvert % 3 === 0 || integerToConvert.toString().indexOf("3") > -1);
     },
 
-    shouldBeConvertedToBuzz: function(integerToConvert) {
-        return (integerToConvert%5 === 0 || integerToConvert.toString().indexOf("5") > -1);
+    shouldBeConvertedToBuzz: function (integerToConvert) {
+        return (integerToConvert % 5 === 0 || integerToConvert.toString().indexOf("5") > -1);
     },
 
-    convertRangeOfIntegers: function(upperLimit) {
-        for(var i = 1; i <= upperLimit; i++) {
+    convertRangeOfIntegers: function (upperLimit) {
+        for (var i = 1; i <= upperLimit; i++) {
             console.log(this.convertSingleInteger(i));
         }
     }
-}
-
+};
 
 module.exports = FizzBuzz;
-
-//FizzBuzz.convertRangeOfIntegers(100);
